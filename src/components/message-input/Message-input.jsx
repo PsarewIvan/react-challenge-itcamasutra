3e2f1099bb75e92b6f963c25bcd7b843f59f0d32
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-15 15:06:20 +0300

    Добавляет событие на кнопку отправки сообщения

--- src/components/message-input/Message-input.jsx
+++ src/components/message-input/Message-input.jsx
@@ -1,6 +1,13 @@
+import React from 'react';
 import './Message-input.css';
 
 const MessageInput = () => {
+  const input = React.createRef();
+  const handleButtonClick = (evt) => {
+    evt.preventDefault();
+    console.log(input.current.value);
+  };
+
   return (
     <form className="message-input" name="message-input">
       <textarea
@@ -8,8 +15,13 @@ const MessageInput = () => {
         type="text"
         name="post"
         placeholder="Write a message..."
+        ref={input}
       ></textarea>
-      <button className="message-input__button" type="submit">
+      <button
+        className="message-input__button"
+        type="submit"
+        onClick={handleButtonClick}
+      >
         Send
       </button>
     </form>
