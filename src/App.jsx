3c1c48c967e8fd35fe6993acb8b8d463453e054f
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-16 10:08:28 +0300

    Выносит Router в index

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 function App(props) {
   return (
-    <Router>
-      <div className="app__wrapper">
-        <div className="app__header">
-          <Header
-            profileItems={props.data.profileItems}
-            users={props.data.users}
-            currentUserId={props.data.currentUserId}
-          />
-        </div>
-        <div className="app__content">
-          <Content
-            currentUserId={props.data.currentUserId}
-            users={props.data.users}
-            messages={props.data.messages}
-            dialogs={props.data.dialogs}
-            posts={props.data.posts}
-            userMessageText={props.data.userMessageText}
-            messagePlaceholder={props.data.messagePlaceholder}
-            dispatch={props.dispatch}
-          />
-        </div>
+    <div className="app__wrapper">
+      <div className="app__header">
+        <Header
+          profileItems={props.data.profileItems}
+          users={props.data.users}
+          currentUserId={props.data.currentUserId}
+        />
       </div>
-    </Router>
+      <div className="app__content">
+        <Content
+          currentUserId={props.data.currentUserId}
+          users={props.data.users}
+          messages={props.data.messages}
+          dialogs={props.data.dialogs}
+          posts={props.data.posts}
+          userMessageText={props.data.userMessageText}
+          messagePlaceholder={props.data.messagePlaceholder}
+          dispatch={props.dispatch}
+        />
+      </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { Store } from './store/Store';
 import 'normalize.css';
@@ -8,7 +9,9 @@ import './index.css';
 const renderApp = (state, dispatch) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App data={state} dispatch={dispatch} />
+      <Router>
+        <App data={state} dispatch={dispatch} />
+      </Router>
     </React.StrictMode>,
     document.getElementById('root')
   );
