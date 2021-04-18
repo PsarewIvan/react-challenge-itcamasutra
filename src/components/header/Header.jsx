140ba60d62faf1fe3599ce624e90ed3804ff4d38
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-18 21:21:10 +0300

    Выносит стили фона карточки в ее обертку

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,12 +9,14 @@ const Header = (props) => {
       <div className="header__logo">
         <Logo />
       </div>
-      <div className="header__profile">
-        <UserCard
-          userName={props.userName}
-          userStatus={props.userStatus}
-          userConnections={props.userConnections}
-        />
+      <div className="header__profile-wrapper">
+        <div className="header__profile">
+          <UserCard
+            userName={props.userName}
+            userStatus={props.userStatus}
+            userConnections={props.userConnections}
+          />
+        </div>
       </div>
       <div className="header__nav">
         <Nav />
