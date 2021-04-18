65648ae575d02103c5c26aedbfaffbb2c3b57fd5
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-18 20:47:32 +0300

    Создает редъюсер страницы пользователей

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,16 +1,118 @@
-const initialState = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Sam Sam' },
-  { id: 3, name: 'Admin' },
-  { id: 4, name: 'Alexander Alexandrovich' },
-  { id: 5, name: 'Superman' },
-  { id: 6, name: 'Batman' },
-  { id: 7, name: 'Ken Lee' },
-  { id: 8, name: 'Lori Chase' },
-];
+const FOLLOW = 'FOLLOW';
+const UN_FOLLOW = 'UN_FOLLOW';
+const SET_USERS = 'GET_USERS';
 
-const usersReducer = (state = initialState, action) => {
-  return state;
+const initialState = {
+  usersCount: 15,
+  friendsCount: 1,
+  users: [
+    {
+      id: 2,
+      name: 'Sam Sam',
+      lastActive: 'Active 56 minutes ago',
+      userConnections: [
+        { id: 1, name: 'Friends', count: 4 },
+        { id: 2, name: 'Groups', count: 1 },
+      ],
+      isFollowed: true,
+    },
+    {
+      id: 3,
+      name: 'Admin',
+      lastActive: 'Active 5 months, 1 week ago',
+      userConnections: [
+        { id: 1, name: 'Friends', count: 10 },
+        { id: 2, name: 'Groups', count: 2 },
+      ],
+      isFollowed: true,
+    },
+    {
+      id: 4,
+      name: 'Alexander Alexandrovich',
+      lastActive: 'Active 5 months, 3 weeks ago',
+      userConnections: [
+        { id: 1, name: 'Friends', count: 17 },
+        { id: 2, name: 'Groups', count: 3 },
+      ],
+      isFollowed: false,
+    },
+    {
+      id: 5,
+      name: 'Superman',
+      lastActive: 'Active 5 months, 3 weeks ago',
+      userConnections: [
+        { id: 1, name: 'Friends', count: 1 },
+        { id: 2, name: 'Groups', count: 8 },
+      ],
+      isFollowed: false,
+    },
+    {
+      id: 6,
+      name: 'Batman',
+      lastActive: 'Active 5 months, 3 weeks ago',
+      userConnections: [
+        { id: 1, name: 'Friends', count: 14 },
+        { id: 2, name: 'Groups', count: 31 },
+      ],
+      isFollowed: true,
+    },
+    {
+      id: 7,
+      name: 'Ken Lee',
+      lastActive: 'Active 5 months, 3 weeks ago',
+      userConnections: [
+        { id: 1, name: 'Friends', count: 12 },
+        { id: 2, name: 'Groups', count: 23 },
+      ],
+      isFollowed: false,
+    },
+    {
+      id: 8,
+      name: 'Lori Chase',
+      lastActive: 'Active 5 months, 3 weeks ago',
+      userConnections: [
+        { id: 1, name: 'Friends', count: 6 },
+        { id: 2, name: 'Groups', count: 41 },
+      ],
+      isFollowed: false,
+    },
+  ],
 };
 
-export { usersReducer };
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FOLLOW:
+      return {
+        ...state,
+        users: state.users.map((user) => {
+          if (user.id === action.id) {
+            return { ...user, isFollowed: true };
+          }
+          return user;
+        }),
+      };
+
+    case UN_FOLLOW:
+      return {
+        ...state,
+        users: state.users.map((user) => {
+          if (user.id === action.id) {
+            return { ...user, isFollowed: false };
+          }
+          return user;
+        }),
+      };
+
+    case SET_USERS:
+      return { ...state, users: [...state.users, ...action.users] };
+
+    default:
+      return state;
+  }
+};
+
+const followCreator = (id) => ({ type: FOLLOW, id });
+const unFollowCreator = (id) => ({ type: UN_FOLLOW, id });
+const setUsersCreator = (users) => ({ type: SET_USERS, users });
+
+export { usersReducer, followCreator, unFollowCreator, setUsersCreator };
