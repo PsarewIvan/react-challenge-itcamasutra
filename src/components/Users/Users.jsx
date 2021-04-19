6352fc6d2bcb882dea65b2e2b4926b74fafcd1e6
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-19 20:44:18 +0300

    Выносит логику запросов на сервер в контейнерную компоненту

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,122 +1,53 @@
 import React from 'react';
 import User from './../User/User';
 import UsersSort from './../UsersSort/UsersSort';
-import UsersPagination from './../UsersPagination/UsersPagination';
-import * as axios from 'axios';
+import UsersPaginationButton from './../UsersPaginationButton/UsersPaginationButton';
 import './Users.css';
 
-class Users extends React.Component {
-  componentDidMount() {
-    this.serverRequest(this.props.currentPage);
-  }
-
-  serverRequest(pageNumber) {
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        this.props.setNumberOfPages(response.data.totalCount);
-        this.props.setUsers(response.data.items);
-      });
-  }
-
-  changeUserPage = (pageNumber) => {
-    this.props.changeCurrentPage(pageNumber);
-    this.serverRequest(pageNumber);
-  };
-
-  buttonsRender() {
-    const pageCount = Math.ceil(this.props.totalCount / this.props.pageSize);
+const Users = (props) => {
+  const buttonsRender = () => {
+    const pageCount = Math.ceil(props.totalCount / props.pageSize);
     const buttons = [];
     for (let i = 1; i <= pageCount; i++) {
       buttons.push(i);
     }
     return buttons.map((i) => (
-      <UsersPagination
+      <UsersPaginationButton
         number={i}
-        currentPage={this.props.currentPage}
-        changePage={this.changeUserPage}
+        currentPage={props.currentPage}
+        changePage={props.changePage}
         key={i}
       />
     ));
-  }
+  };
 
-  render() {
-    return (
-      <div className="users">
-        <div className="users__sort">
-          <UsersSort
-            friendsCount={'this.props.friendsCount'}
-            usersCount={this.props.totalCount}
-          />
-        </div>
-        <div className="users__pagination">{this.buttonsRender()}</div>
-        <ul className="users__list">
-          {this.props.users.map((user) => {
-            return (
-              <li className="users__user" key={user.id}>
-                <User
-                  userConnections={user.userConnections}
-                  name={user.name}
-                  lastActive={user.lastActive}
-                  isFollowed={user.isFollowed}
-                  photos={user.photos}
-                  id={user.id}
-                  follow={this.props.follow}
-                  unFollow={this.props.unFollow}
-                />
-              </li>
-            );
-          })}
-        </ul>
+  return (
+    <div className="users">
+      <div className="users__sort">
+        <UsersSort
+          friendsCount={'this.props.friendsCount'}
+          usersCount={props.totalCount}
+        />
       </div>
-    );
-  }
-}
-
-// const UsersE = (props) => {
-//   const getUsers = () => {
-//     if (props.users && props.users.length === 0) {
-//       axios
-//         .get('https://social-network.samuraijs.com/api/1.0/users')
-//         .then((response) => {
-//           props.setUsers(response.data.items);
-//         });
-//     }
-//   };
-
-//   return (
-//     <div className="users">
-//       <button type="button" onClick={getUsers}>
-//         Get users
-//       </button>
-//       <div className="users__sort">
-//         <UsersSort
-//           friendsCount={props.friendsCount}
-//           usersCount={props.usersCount}
-//         />
-//       </div>
-//       <ul className="users__list">
-//         {props.users.map((user) => {
-//           return (
-//             <li className="users__user" key={user.id}>
-//               <User
-//                 userConnections={user.userConnections}
-//                 name={user.name}
-//                 lastActive={user.lastActive}
-//                 isFollowed={user.isFollowed}
-//                 photos={user.photos}
-//                 id={user.id}
-//                 follow={props.follow}
-//                 unFollow={props.unFollow}
-//               />
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </div>
-//   );
-// };
+      <div className="users__pagination">{buttonsRender()}</div>
+      <ul className="users__list">
+        {props.users.map((user) => (
+          <li className="users__user" key={user.id}>
+            <User
+              userConnections={user.userConnections}
+              name={user.name}
+              lastActive={user.lastActive}
+              isFollowed={user.isFollowed}
+              photos={user.photos}
+              id={user.id}
+              follow={props.follow}
+              unFollow={props.unFollow}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import * as axios from 'axios';
 import Users from './Users';
 import {
   followCreator,
@@ -42,6 +44,40 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    this.serverRequest(this.props.currentPage);
+  }
 
-export default UsersContainer;
+  serverRequest(pageNumber) {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setNumberOfPages(response.data.totalCount);
+        this.props.setUsers(response.data.items);
+      });
+  }
+
+  changeUserPage = (pageNumber) => {
+    this.props.changeCurrentPage(pageNumber);
+    this.serverRequest(pageNumber);
+  };
+
+  render() {
+    return (
+      <Users
+        totalCount={this.props.totalCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        changePage={this.changeUserPage}
+        users={this.props.users}
+        follow={this.props.follow}
+        unFollow={this.props.unFollow}
+      />
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
