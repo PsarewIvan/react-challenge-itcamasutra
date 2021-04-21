edda524ce474181cddbfe174406779b530f69d1b
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-21 15:25:34 +0300

    Стилизует кнопки настроек пользователя

--- src/components/ProfileEditButton/ProfileEditButton.jsx
+++ src/components/ProfileEditButton/ProfileEditButton.jsx
@@ -1,7 +1,22 @@
 import './ProfileEditButton.css';
+import editLogo from './img/edit.svg';
+import backgroundLogo from './img/background.svg';
 
-const ProfileEditButton = () => {
-  return <div className="profile-edit-button">Button</div>;
+const ProfileEditButton = (props) => {
+  let icon, altText;
+  if (props.type === 'settings') {
+    icon = editLogo;
+    altText = 'Change settings';
+  }
+  if (props.type === 'background') {
+    icon = backgroundLogo;
+    altText = 'Change background image';
+  }
+  return (
+    <a className="profile-edit-button" href="/mock-link/">
+      <img className="profile-edit-button__icon" src={icon} alt={altText} />
+    </a>
+  );
 };
 
 export default ProfileEditButton;
