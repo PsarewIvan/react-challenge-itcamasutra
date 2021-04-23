aad0130dbfd392ddb6d9386a03c278b6d35c389e
Ivan Psarew <psarew.ivan@yandex.ru>
2021-04-23 10:23:45 +0300

    Добавляет basename в роутер

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store';
-import App from './App';
+import AppContainer from './AppContainer';
 import 'normalize.css';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/react-challenge-itcamasutra/">
       <Provider store={store}>
-        <App />
+        <AppContainer />
       </Provider>
     </Router>
   </React.StrictMode>,
