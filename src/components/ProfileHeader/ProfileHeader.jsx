6dd8ec9501949e95dc3acb1af46f2befaf0d273c
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-06 18:47:00 +0300

    Правит пропс статуса

--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -15,7 +15,7 @@ const ProfileHeader = (props) => {
         <p className="profile-header__user-name">{props.fullName}</p>
       </div>
       <div className="profile-header__about">
-        <ProfileStatus about={`props.about`} />
+        <ProfileStatus about={props.about} />
       </div>
       <div className="profile-header__edit">
         <ProfileEditButton type="settings" />
