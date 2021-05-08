b7b1c3569b08eef78c30e0ca1bef7bc5222e1f58
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-08 17:15:20 +0300

    Объединяет компоненты статуса, дорабатывает логику

--- src/components/ProfileStatus/ProfileStatus.jsx
+++ src/components/ProfileStatus/ProfileStatus.jsx
@@ -1,24 +1,76 @@
+import React from 'react';
 import './ProfileStatus.css';
 
-const ProfileStatus = (props) => {
-  return (
-    <div className="profile-status">
-      {!props.isChanging && (
-        <div className="profile-status__text" onClick={props.startChange}>
-          {props.about}
-        </div>
-      )}
-      {props.isChanging && (
-        <input
-          className="profile-status__input"
-          type="text"
-          value={props.about}
-          autoFocus={true}
-          onBlur={props.endChange}
-        />
-      )}
-    </div>
-  );
-};
+class ProfileStatus extends React.Component {
+  state = {
+    isStatusChanging: false,
+    status: this.props.status,
+  };
+
+  onStartChange = () => {
+    this.setState({ isStatusChanging: true });
+  };
+
+  onEndChange = () => {
+    this.setState({ isStatusChanging: false });
+  };
+
+  onUpdateStatus = () => {
+    if (this.props.status !== this.state.status) {
+      this.props.updateStatus(this.state.status);
+    }
+    this.setState({ isStatusChanging: false });
+  };
+
+  onStatusChange = (evt) => {
+    this.setState({
+      status: evt.target.value,
+    });
+  };
+
+  componentDidUpdate() {
+    console.log('update');
+    if (this.state.status !== this.props.status) {
+      this.setState({ status: this.props.status });
+    }
+  }
+
+  render() {
+    return (
+      <div className="profile-status">
+        {!this.state.isStatusChanging && (
+          <div className="profile-status__text" onClick={this.onStartChange}>
+            {this.props.status || 'Change me'}
+          </div>
+        )}
+        {this.state.isStatusChanging && (
+          <div className="profile-status__editor">
+            <input
+              className="profile-status__input"
+              type="text"
+              value={this.state.status}
+              autoFocus={true}
+              onChange={this.onStatusChange}
+            />
+            <button
+              className="profile-status__button"
+              type="button"
+              onClick={this.onUpdateStatus}
+            >
+              Change status
+            </button>
+            <button
+              className="profile-status__button profile-status__button--cancel"
+              type="button"
+              onClick={this.onEndChange}
+            >
+              Close
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default ProfileStatus;
