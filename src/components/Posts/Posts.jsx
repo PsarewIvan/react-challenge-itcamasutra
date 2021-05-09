51b52313274011d68b51286082f9382042d4b2d4
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-09 14:25:20 +0300

    Меняет порядок отрисовки постов

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,18 +2,21 @@ import Post from '../Post/Post';
 import './Posts.css';
 
 const Posts = (props) => {
-  const postElement = props.posts.map((post) => {
-    return (
-      <li className="posts__element" key={`posts-${post.id}`}>
-        <Post
-          message={post.message}
-          likeCount={post.likeCount}
-          userName={props.userName}
-          photos={props.photos}
-        />
-      </li>
-    );
-  });
+  console.log(props.posts);
+  const postElement = props.posts
+    .map((post) => {
+      return (
+        <li className="posts__element" key={`posts-${post.id}`}>
+          <Post
+            message={post.message}
+            likeCount={post.likeCount}
+            userName={props.userName}
+            photos={props.photos}
+          />
+        </li>
+      );
+    })
+    .reverse();
 
   return <ul className="posts">{postElement}</ul>;
 };
