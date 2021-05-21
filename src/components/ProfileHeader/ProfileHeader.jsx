89dd90adf1aa9440d0530665eb2be3b56d8ae2a5
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-22 00:54:26 +0300

    Добовляет в ProfileHeader  загрузку аватарок

--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,17 +1,35 @@
+import { useDispatch } from 'react-redux';
+import { changePhoto } from './../../redux/profile-reducer';
 import ProfileStatus from '../ProfileStatus/ProfileStatus';
 import ProfileEditButton from './../ProfileEditButton/ProfileEditButton';
 import userPhoto from './../../assets/img/avatar/avatar.jpeg';
-import './ProfileHeader.css';
+import './ProfileHeader.scss';
 
 const ProfileHeader = ({ photo, fullName, status, updateStatus }) => {
+  const dispatch = useDispatch();
+  const handelInputChange = (evt) => {
+    dispatch(changePhoto(evt.target.files[0]));
+  };
+
   return (
     <div className="profile-header">
       <div className="profile-header__avatar">
-        <img
-          className="profile-header__avatar-img"
-          src={!photo ? userPhoto : photo}
-          alt="User's avatar"
-        />
+        <div className="profile-header__img-wrapper">
+          <img
+            className="profile-header__avatar-img"
+            src={!photo ? userPhoto : photo}
+            alt="User's avatar"
+          />
+          <label className="profile-header__label-img" title="Сменить фото">
+            <input
+              className="profile-header__input-img"
+              type="file"
+              aria-label="Загрузить фото для аватара"
+              placeholder=""
+              onChange={handelInputChange}
+            ></input>
+          </label>
+        </div>
         <p className="profile-header__user-name">{fullName}</p>
       </div>
       <div className="profile-header__about">
