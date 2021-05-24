a937cedf688f82c7bde6f56dea0238f1aa2e1241
Ivan Psarew <psarew.ivan@yandex.ru>
2021-05-24 20:14:27 +0300

    Подключает redux devtools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { headerReducer } from './header-reducer';
 import { communicationReducer } from './communication-reducer';
@@ -16,6 +16,7 @@ const reducers = combineReducers({
   app: appReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
